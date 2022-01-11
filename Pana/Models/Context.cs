e92f00769271a77b0346ed96852edcd27812db25
JoseAugustoJasfarias <jasfarias@hotmail.com>
2022-01-10 21:24:05 -0300

    Publicando Código Aberto do Projeto Pana

---
+++ Pana/Models/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pana.Models;
+
+namespace Pana.Models
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+            //Database.EnsureCreated();
+        }
+        public DbSet<Contato> Contatos { get; set; }
+        public DbSet<ONG> ONGs { get; set; }
+        public DbSet<Login> Logins { get; set; }
+    }
+
+}
---
+++ Pana/Models/Login.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Pana.Models
+{
+    public class Login
+    {
+
+
+        [Key]
+        public int IdLogin { get; set; }
+
+        public string Usuario { get; set; }
+        
+        public string Senha { get; set; }
+
+
+    }
+
+}
